53892d3617f189178eea5e75a201e4e2e00afba5
Zahid Hasan <pzahidhasan@outlook.com>
2022-06-27 10:27:55 +0600

    gold strings

--- main.go
+++ main.go
@@ -14,5 +14,10 @@ func main() {
 		phrases = append(phrases, faker.Hacker().Phrases()...)
 	}
 
-	fmt.Println(strings.Join(phrases[:], "; "))
+	output := strings.Join(phrases[:], "; ")
+	r, g, b := 255, 215, 0 // gold color
+
+	for j := 0; j < len(output); j++ {
+		fmt.Printf("\033[38;2;%d;%d;%dm%c\033[0m", r, g, b, output[j])
+	}
 }

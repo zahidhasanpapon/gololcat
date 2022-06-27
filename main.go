46edde60d11cdb598fc458e5d4a485d577cf9daa
Zahid Hasan <pzahidhasan@outlook.com>
2022-06-27 10:31:53 +0600

    rainbow strings

--- main.go
+++ main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strings"
 
 	"syreclabs.com/go/faker"
 )
+
+func rgb(i int) (int, int, int) {
+	var f = 0.1
+	return int(math.Sin(f*float64(i)+0)*127 + 128),
+		int(math.Sin(f*float64(i)+2*math.Pi/3)*127 + 128),
+		int(math.Sin(f*float64(i)+4*math.Pi/3)*127 + 128)
+}
 
 func main() {
 	var phrases []string
@@ -15,9 +23,9 @@ func main() {
 	}
 
 	output := strings.Join(phrases[:], "; ")
-	r, g, b := 255, 215, 0 // gold color
 
 	for j := 0; j < len(output); j++ {
+		r, g, b := rgb(j)
 		fmt.Printf("\033[38;2;%d;%d;%dm%c\033[0m", r, g, b, output[j])
 	}
 }
